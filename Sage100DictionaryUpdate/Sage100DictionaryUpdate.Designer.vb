c66219df22e862cc1cde7572c616603cf2568368
Ali Esfahani <ali.esfahani@sage.com>
2017-01-27 07:13:11 -0800

    Added Sage 100 Disctionary Update

---
+++ Sage100DictionaryUpdate/Sage100DictionaryUpdate.Designer.vb
@@ -0,0 +1,313 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Sage100DictionaryUpdate
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Sage100DictionaryUpdate))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.TextBox3 = New System.Windows.Forms.TextBox
+        Me.USERNAME = New System.Windows.Forms.Label
+        Me.Password = New System.Windows.Forms.Label
+        Me.TextBox4 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox
+        Me.Label13 = New System.Windows.Forms.Label
+        Me.Label12 = New System.Windows.Forms.Label
+        Me.Button7 = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HowToUserToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AboutSage100DictionaryUpdateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Result = New System.Windows.Forms.ListBox
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Green
+        Me.Button1.Location = New System.Drawing.Point(473, 78)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 77)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Update"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(15, 105)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(15, 154)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 4
+        '
+        'USERNAME
+        '
+        Me.USERNAME.AutoSize = True
+        Me.USERNAME.Location = New System.Drawing.Point(12, 82)
+        Me.USERNAME.Name = "USERNAME"
+        Me.USERNAME.Size = New System.Drawing.Size(137, 13)
+        Me.USERNAME.TabIndex = 5
+        Me.USERNAME.Text = "Step 2 : Enter a User Name"
+        '
+        'Password
+        '
+        Me.Password.AutoSize = True
+        Me.Password.Location = New System.Drawing.Point(12, 132)
+        Me.Password.Name = "Password"
+        Me.Password.Size = New System.Drawing.Size(130, 13)
+        Me.Password.TabIndex = 6
+        Me.Password.Text = "Step 3 : Enter a Password"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(15, 198)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 178)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Step 4 : Enter a Company Code"
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.CheckOnClick = True
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"AP", "AR", "BC", "BM", "BR", "CI", "ES", "FL", "GL", "IM", "IT", "JC", "MP", "MR", "PO", "PR", "RA", "SO", "TC", "WO"})
+        Me.CheckedListBox1.Location = New System.Drawing.Point(191, 79)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(175, 139)
+        Me.CheckedListBox1.TabIndex = 14
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(400, 114)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(69, 13)
+        Me.Label13.TabIndex = 26
+        Me.Label13.Text = "(Deselect All)"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(400, 82)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(57, 13)
+        Me.Label12.TabIndex = 25
+        Me.Label12.Text = "(Select All)"
+        '
+        'Button7
+        '
+        Me.Button7.BackgroundImage = CType(resources.GetObject("Button7.BackgroundImage"), System.Drawing.Image)
+        Me.Button7.Location = New System.Drawing.Point(371, 109)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(23, 23)
+        Me.Button7.TabIndex = 24
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.BackgroundImage = CType(resources.GetObject("Button5.BackgroundImage"), System.Drawing.Image)
+        Me.Button5.Location = New System.Drawing.Point(371, 78)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(23, 23)
+        Me.Button5.TabIndex = 23
+        Me.Button5.Text = "CL"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(619, 24)
+        Me.MenuStrip1.TabIndex = 28
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(35, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HowToUserToolStripMenuItem, Me.AboutSage100DictionaryUpdateToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(40, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'HowToUserToolStripMenuItem
+        '
+        Me.HowToUserToolStripMenuItem.Name = "HowToUserToolStripMenuItem"
+        Me.HowToUserToolStripMenuItem.Size = New System.Drawing.Size(251, 22)
+        Me.HowToUserToolStripMenuItem.Text = "How to use"
+        '
+        'AboutSage100DictionaryUpdateToolStripMenuItem
+        '
+        Me.AboutSage100DictionaryUpdateToolStripMenuItem.Name = "AboutSage100DictionaryUpdateToolStripMenuItem"
+        Me.AboutSage100DictionaryUpdateToolStripMenuItem.Size = New System.Drawing.Size(251, 22)
+        Me.AboutSage100DictionaryUpdateToolStripMenuItem.Text = "About Sage 100 Dictionary Update"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 229)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 29
+        Me.Label2.Text = "Result Log"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(506, 168)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(47, 23)
+        Me.Button2.TabIndex = 30
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(15, 52)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(362, 20)
+        Me.TextBox1.TabIndex = 31
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 33)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(383, 13)
+        Me.Label3.TabIndex = 32
+        Me.Label3.Text = "This is the path to MAS90\Home folder that was retrieved from your registry key "
+        '
+        'Result
+        '
+        Me.Result.FormattingEnabled = True
+        Me.Result.HorizontalScrollbar = True
+        Me.Result.Location = New System.Drawing.Point(15, 248)
+        Me.Result.Name = "Result"
+        Me.Result.ScrollAlwaysVisible = True
+        Me.Result.Size = New System.Drawing.Size(592, 225)
+        Me.Result.TabIndex = 33
+        '
+        'Sage100DictionaryUpdate
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(619, 486)
+        Me.Controls.Add(Me.Result)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Password)
+        Me.Controls.Add(Me.USERNAME)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Sage100DictionaryUpdate"
+        Me.Text = "Sage 100 Dictionary Update"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents USERNAME As System.Windows.Forms.Label
+    Friend WithEvents Password As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents CheckedListBox1 As System.Windows.Forms.CheckedListBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HowToUserToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutSage100DictionaryUpdateToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Result As System.Windows.Forms.ListBox
+
+End Class
---
+++ Sage100DictionaryUpdate/Sage100DictionaryUpdate.vb
@@ -0,0 +1,353 @@
+﻿Imports ProvideX.ScriptClass
+'Imports Outlook = Microsoft.Office.Interop.Outlook
+Public Class Sage100DictionaryUpdate
+
+    Inherits System.Windows.Forms.Form
+    Public retVAL As Integer
+    Public InitPath As String
+    Public PvxScript As ProvideX.Script
+    Public myPVXObject As ProvideX.PvxDispatch
+    Public oUI As ProvideX.PvxDispatch
+    Public Embed As ProvideX.PvxDispatch
+    Public mySecur As ProvideX.PvxDispatch
+    Dim LastClickTick As Integer = 0
+    Public Howmany As Integer
+    Public Maybe As String = ""
+    Public er As Integer = 0
+    Public Done As Integer = 0
+
+
+
+ 
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'Dim path As String = ""
+
+        'path = TextBox1.Text
+
+
+
+
+
+
+
+        'If Len(path) = 0 Then
+        'MsgBox("Sorry the program could not retrieve the path to your MAS90 folder " & "Please enter it manually")
+        ' er = 1
+        'GoTo endd2
+        'End If
+
+        ' Try
+        'Dim SOTAMAS90Key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\ODBC\ODBC.INI\SOTAMAS90")
+        '  Dim InitPath2 = SOTAMAS90Key.GetValue("Directory")
+        'TextBox1.Text = InitPath2 + "\Home"
+        ' Catch ex As Exception
+        'MsgBox("Sorry the program could not retrieve the path to your MAS90 folder" & "Please enter it manually")
+        'er = 1
+        'GoTo endd2
+        ' End Try
+
+        ' If Howmany = 1 Then
+        'If Maybe = "" Then
+
+
+
+        'Try
+        'Dim SOTAMAS90Key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\ODBC\ODBC.INI\SOTAMAS90")
+        ' Dim InitPath = SOTAMAS90Key.GetValue("Directory") & "\Home"
+
+
+        ' Catch ex As Exception
+        ' MsgBox("There is something wrong setting the SY_session object" & vbCrLf & " Please send an email to ali.esfahani@sage.com with steps to duplicate " & ex.Message)
+        'er = 1
+        ' GoTo endd2
+        ' End Try
+        ' End If
+        ' If Maybe = "Y" Then
+        'Try
+        'InitPath = TextBox1.Text
+        ' PvxScript = New ProvideX.Script
+        ' PvxScript.Init(InitPath)
+        ' myPVXObject = PvxScript.NewObject("SY_Session")
+        'Catch ex As Exception
+        'MsgBox("There is something wrong setting the SY_session object" & vbCrLf & " Please send an email to ali.esfahani@sage.com with steps to duplicate " & ex.Message)
+        ' er = 1
+        ' GoTo endd2
+        ' End Try
+        ' End If
+        '  End If
+
+
+
+        Try
+            If System.Environment.TickCount - LastClickTick < 1000 Then
+                Exit Sub
+            Else
+
+                'Clear the List
+                Me.Result.Items.Clear()
+
+                'Declearing variables
+                Dim User, Password, Company, Mo As String
+
+
+
+                ' Variable defenition 
+                User = TextBox2.Text
+                Password = TextBox3.Text
+                Company = TextBox4.Text
+                Company = UCase(Company)
+
+                If Company = "" Then
+                    MsgBox("Please Enter a Valid Company code")
+                    er = 1
+                    GoTo Endd
+                End If
+                If Len(Password) = 0 Then
+                    Password = ""
+                End If
+
+                If CheckedListBox1.CheckedItems.Count = 0 Then
+                    MsgBox("Please select at least one module to be updated")
+                    er = 1
+                    GoTo Endd
+
+                End If
+                retVAL = myPVXObject.nSetUser(User, Password)
+
+
+                If retVAL = 0 Then
+                    MsgBox("Unable to use this user ID and password because " & myPVXObject.sLastErrorMsg)
+                    er = 1
+
+                    GoTo Endd
+                End If
+                retVAL = myPVXObject.nSetCompany(Company, 1)
+                If retVAL = 0 Then
+                    MsgBox("Unable to set company " & Company & " Because " & myPVXObject.sLastErrorMsg & vbCrLf & "Try check marking the Allow External Access in Company maintenance preferences tab")
+                    er = 1
+                    GoTo Endd
+
+                End If
+
+                retVAL = myPVXObject.nSetModule("SYS")
+                If retVAL = 0 Then
+                    MsgBox("Unable to set module SYS" & myPVXObject.sLastErrorMsg)
+                    er = 1
+                    GoTo Endd
+                End If
+
+                Try
+                    mySecur = PvxScript.NewObject("SY_Security", myPVXObject, 0)
+                Catch ex As Exception
+                    MsgBox("Sorry was not able to set the security object for this user because ", ex.Message)
+                    er = 1
+                    GoTo Endd
+                End Try
+
+                retVAL = myPVXObject.nSetProgram(myPVXObject.nLookupTask("SY_company_ui"))
+
+                If retVAL <> 0 Then
+                    Embed = PvxScript.NewObject("SY_activation_bus", myPVXObject)
+                Else
+                    MsgBox("Sorry Unable to Set the Activation BUS object because " & myPVXObject.sLastErrorMsg)
+                    er = 1
+                    GoTo Endd
+                End If
+                Result.Items.Add("The Path to MAS90 folder that we are using for this update is " & TextBox1.Text)
+                Result.Refresh()
+                For Each itemChecked In CheckedListBox1.CheckedItems
+                    Mo = itemChecked
+                    Mo = Mo.Substring(0, 1) + "/" + Mo.Substring(1, 1)
+
+                    Result.Items.Add("Running Dictionary Update(Embedded I/O) for module " & itemChecked & " ...")
+                    Done = 1
+                    Result.Refresh()
+                    retVAL = Embed.nActivate(Mo, Company)
+                    If retVAL <> 0 Then
+                        Result.Items.Add("Success. ")
+                        Done = 1
+                    Else
+                        Result.Items.Add("Failed, and last error message is " & Embed.sLastErrorMsg)
+                        Result.Refresh()
+                        MsgBox("Dictionary Update for Company " & Company & " and Module " & Mo & " has failed and the last error is:" & vbCrLf & Embed.sLastErrorMsg & vbCrLf & vbCrLf & " Update process will stop now.")
+                        er = 1
+                        GoTo Endd
+
+                    End If
+
+
+
+                Next
+            End If
+Endd:
+        Finally
+            LastClickTick = System.Environment.TickCount
+        End Try
+endd2:
+        If er = 0 Then
+            Result.Items.Add("Dictionary update for requested modules is done. Thank you ")
+        End If
+        Howmany = Howmany + 1
+
+    End Sub
+
+
+
+   
+    Public Sub Sage100DictionaryUpdate_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Howmany = 1
+        Try
+
+            Dim SOTAMAS90Key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\ODBC\ODBC.INI\SOTAMAS90")
+            Dim InitPath2 = SOTAMAS90Key.GetValue("Directory")
+            TextBox1.Text = InitPath2 + "\Home"
+            InitPath2 = TextBox1.Text
+
+            PvxScript = New ProvideX.Script
+            PvxScript.Init(InitPath2)
+            myPVXObject = PvxScript.NewObject("SY_Session")
+
+        Catch ex As Exception
+            MsgBox("Sorry the program could not retrieve the path to your MAS90\Home folder " & "Please close the program and first log in MAS90 or 200 and then launch this application." & vbCrLf & "Note: At the moment this application does not work with MAS200 SQL")
+            Maybe = "Y"
+
+        End Try
+
+
+
+
+
+    End Sub
+
+    Private Sub Sage100DictionaryUpdate_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+        If er <> 1 Then
+            If Done = 1 Then
+                myPVXObject.ncleanup()
+                myPVXObject.DropObject()
+                PvxScript = Nothing
+                myPVXObject = Nothing
+            End If
+          
+        End If
+    End Sub
+
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        For I = 0 To CheckedListBox1.Items.Count - 1
+            CheckedListBox1.SetItemChecked(I, True)
+        Next
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        For I = 0 To CheckedListBox1.Items.Count - 1
+
+            CheckedListBox1.SetItemChecked(I, False)
+
+        Next
+    End Sub
+
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        For I = 0 To CheckedListBox1.Items.Count - 1
+
+            CheckedListBox1.SetItemChecked(I, False)
+
+        Next
+        Me.Result.Items.Clear()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        myPVXObject.ncleanup()
+        myPVXObject.DropObject()
+        PvxScript = Nothing
+        myPVXObject = Nothing
+        Me.Close()
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClearToolStripMenuItem.Click
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        For I = 0 To CheckedListBox1.Items.Count - 1
+
+            CheckedListBox1.SetItemChecked(I, False)
+
+        Next
+        Me.Result.Items.Clear()
+    End Sub
+
+    Private Sub AboutSage100DictionaryUpdateToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutSage100DictionaryUpdateToolStripMenuItem.Click
+        Dim oForm As AboutBox1
+
+        oForm = New AboutBox1()
+        oForm.Show()
+        oForm = Nothing
+    End Sub
+
+    'Private Sub ReportIssueOrAskForEnhancmentToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    'Dim objOutlook As Outlook.Application
+    '  Dim objOutlookMsg As Outlook.MailItem
+    '  Dim objOutlookRecip As Outlook.Recipient
+
+
+    '   Try
+    ' Create the Outlook session.
+    '    objOutlook = CreateObject("Outlook.Application")
+
+    ' Create the message.
+    '      objOutlookMsg = objOutlook.CreateItem(0)
+    '    With objOutlookMsg
+    ' Add the To recipient(s) to the message.
+    '    objOutlookRecip = .Recipients.Add("ali.esfahani@sage.com")
+
+    ' Set the Subject, Body, and Importance of the message.
+    '     .Subject = "Enhancement request / Bug Report / Question "
+    '.Body = "This is the body of the message." & vbCrLf & vbCrLf
+
+    ' Resolve each Recipient's name.
+    '  For Each objOutlookRecip In .Recipients
+    '      objOutlookRecip.Resolve()
+    '  Next
+
+    ' Should we display the message before sending?
+    '     .Display()
+    '    .Save()
+    '.Send()
+
+    '     End With
+    '  Catch ex As Exception
+    '     MsgBox("Sorry something went wrong connecting to Microsoft Outlook" + vbCrLf + "Instead please email to ali.esfahani@sage.com with your request")
+    '    GoTo enddd
+    'End Try
+
+
+    'enddd:
+    'End Sub
+
+    Private Sub HowToUserToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HowToUserToolStripMenuItem.Click
+        Dim r As String
+        r = System.AppDomain.CurrentDomain.BaseDirectory()
+
+        System.Diagnostics.Process.Start(r + "\help.htm")
+    End Sub
+
+
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim fbd As New FolderBrowserDialog
+        fbd.RootFolder = Environment.SpecialFolder.MyComputer
+        If fbd.ShowDialog = DialogResult.OK Then
+            TextBox1.Text = fbd.SelectedPath
+        End If
+
+    End Sub
+
+
+End Class
